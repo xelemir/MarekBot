c806bc340b9d3edf5ed457839d9506b4324f3809
Jan <68973133+xelemir@users.noreply.github.com>
2021-10-17 16:20:25 +0200

    Create _storage_.py

---
+++ _storage_.py
@@ -0,0 +1,47 @@
+# quotes
+quotes = []
+quotes.append("Oh mein Gott ich bin so geil!")
+quotes.append("Mein Modegeschmack ist objektiv betrachtet schon der Beste.")
+quotes.append("Junge, bist du eigentlich dumm!?")
+quotes.append("Joe Biden!")
+quotes.append("SHUT UP YOU CUNT, ICH MUSS AUF RATHAUS 10 RUSHEN!")
+
+# Amogus keywords filter
+amogus = []
+amogus.extend(("AMOGUS", "Amogus", "amogus", "aMOGUS"))
+
+# Amongus keyword filter
+amongus = []
+amongus.extend(("Amongus", "AMONGUS", "amongus", "Among Us", "among us", "Among us", "AMONG US"))
+
+# filter offensive words
+filter_list = []
+filter_list.extend(("Blocked_word0", "Blocked_word1", "Blocked_word2"))
+
+# list of insults (these will be sent as DM to the author of a message containing on of the blocked words)
+insult = []
+insult.append("Insult0")
+insult.append("Insult1")
+insult.append("Insult2")
+
+#jkg quotes
+jkg_quotes = []
+jkg_quotes.append("Quote0")
+jkg_quotes.append("Quote1")
+jkg_quotes.append("Quote2")
+jkg_quotes.append("Quote3")
+jkg_quotes.append("Quote4")
+jkg_quotes.append("Quote5")
+jkg_quotes.append("Quote6")
+jkg_quotes.append("Quote7")
+
+# Images
+coc_img = "https://jan.gruettefien.com/media/clash_of_clans_logo.png"
+locked_img = "https://jan.gruettefien.com/media/locked.png"
+offline_img = "https://jan.gruettefien.com/media/offline.png"
+
+keyword0 = "LINK_TO_IMG_0"
+keyword0 = "LINK_TO_IMG_1"
+keyword0 = "LINK_TO_IMG_2"
+keyword0 = "LINK_TO_IMG_3"
+no_keyword = "LINK_TO_IMG"
