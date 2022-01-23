2fdfb23d9661911b002af8981c9e87ef5e712960
Jan <68973133+xelemir@users.noreply.github.com>
2022-01-24 00:03:25 +0100

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,21 @@
+import pylast
+bot_token = "TODO_DISCORD_BOT_TOKEN"
+
+client_id_spotify="TODO_SPOTIFY_CLIENT_ID"
+client_secret_spotify="TODO_SPOTIFY_CLIENT_SECRET"
+
+admin_id = "TODO_DISCORD_USER_ADMIN_ID"
+bot_id = "TODO_DISCORD_BOT_ID"
+
+trusted_servers = ["TODO_TRUSTED_DISCORD_SERVER_ID1", #users on these servers can use all jkg quotes unfiltered
+                   "TODO_TRUSTED_DISCORD_SERVER_ID2",
+                   "TODO_TRUSTED_DISCORD_SERVER_ID3"]
+
+api_key_imagga = "TODO_IMAGGA_API_KEY"
+api_secret_imagga = "TODO_IMAGGA_API_SECRET"
+
+api_key_lastfm = "TODO_LAST_FM_API_KEY"
+api_secret_lastfm = "TODO_LAST_FM_API_SECRET"
+
+username_lastfm = "TODO_LAST_FM_USERNAME"
+password_hash_lastfm = pylast.md5("TODO_LAST_FM_PASSWORD")
